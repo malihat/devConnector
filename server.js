3a1f5be9deef9737f069ba462449d5952979e146
Maliha Tanweer <malihatanweer@gmail.com>
2020-10-12 10:23:34 -0400

    Prepared for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
@@ -9,15 +10,22 @@ app.use(express.json({ extended: false }));
 
 dotenv.config({ path: './config.env' });
 
-// app.get('/', (req, res) => {
-//   res.send('Hello from express');
-// });
 
 // Define routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/post', require('./routes/api/post'));
+
+// Server static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder 
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 // Connect to mongodb database
 mongoose.connect(process.env.DATABASE, {
